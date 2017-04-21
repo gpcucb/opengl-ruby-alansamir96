ab7a8c5cf78c48a1ba9b459c76897b65c95d0f78
Alan <alansamir96@gmail.com>
2017-04-20 20:36:37 -0400

    dibujar triangulo

--- main.rb
+++ main.rb
@@ -2,8 +2,41 @@ require 'opengl'
 require 'glu'
 require 'glut'
 
-include GL
+include Gl
 include Glu
 include Glut
 
 puts("Hola mundo")
+
+def display
+  glClearColor(0.0, 0.0, 0.0, 0.0)
+
+  glClear(GL_COLOR_BUFFER_BIT)
+
+  glMatrixMode(GL_PROJECTION)
+
+  glLoadIdentity
+
+  glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
+
+  glMatrixMode(GL_MODELVIEW)
+
+  glBegin(GL_TRIANGLES)
+    glColor3f(1.0,0.0,0.0)
+    glVertex3f(0.0,0.8,0.0)
+    glColor3f(0.0,1.0,0.0)
+    glVertex3f(-0.6,-0.2,0.0)
+    glColor3f(0.0,0.0,1.0)
+    glVertex3f(0.6,-0.2,0.0)
+  glEnd
+  glFlush
+  glutPostRedisplay
+end
+
+glutInit
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+glutInitWindowSize(500,500)
+glutInitWindowPosition(20,20)
+glutCreateWindow("Hola opengl")
+glutDisplayFunc :display
+glutMainLoop
